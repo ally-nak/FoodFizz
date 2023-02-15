7fec10124697c714c000a7eda9ba50889699e808
Peng Hao Lu <pengluh@gmail.com>
2023-02-14 16:01:55 -0800

    initial setup of navbars

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TopNav from "./components/TopNav";
+import BotNav from "./components/BotNav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopNav />
+      <div style={{ height: "80vh", backgroundColor: "white" }}>
+        <span>CONTENT HERE</span>
+      </div>
+      <BotNav />
     </div>
   );
 }
