2860609a78497daafc6b36c4c1b541c441b4cc59
Peng Hao Lu <pengluh@gmail.com>
2023-02-17 13:42:17 -0800

    post ui issues fixed -- image and text aligning

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -11,22 +11,22 @@ function Post() {
   return (
     <div className="post-wrapper">
       <div className="post-header">
-        <div>
+        <div className="location-wrapper">
           <img src={Pin} alt="location pin" />
           <span className="post-title">FLOMO</span>
         </div>
-        <div>
+        <div className="time-wrapper">
           <span className="post-time">3 min ago</span>
           <img src={ThumbsUp} alt="thumbs up" />
         </div>
       </div>
-      <div className="post-image">
+      <div className="post-content">
         <p className="post-caption">Food at FloMo is pretty GAS TODAY </p>
-        <img src={Flomo} alt="image feed" />
+        <img className="post-image" src={Flomo} alt="post" />
       </div>
       <div className="post-footer">
         <img src={Flag} style={{ height: 25, width: 30 }} alt="Flag" />
-          <img src={Favorite} style={{ height: 25, width: 30 }} alt="Favorite" />
+        <img src={Favorite} style={{ height: 25, width: 30 }} alt="Favorite" />
       </div>
     </div>
   );
