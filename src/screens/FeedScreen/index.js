1b59069483cf5bf0f2f4665611992ffe4a84cd25
zhuojl00 <72666211+zhuojl00@users.noreply.github.com>
2023-02-20 03:05:14 -0800

    updating firebase fetch feed

--- src/screens/FeedScreen/index.js
+++ src/screens/FeedScreen/index.js
@@ -1,43 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import TopNav from "../../components/TopNav";
 import BotNav from "../../components/BotNav";
 import Post from "../../components/Post";
 import "./feed.css";
 
 import { firestore } from "../../firebase";
-import { getDocs, collection } from "firebase/firestore"
-
-async function retrievePosts() {
-  const querySnapshot = await getDocs(collection(firestore, "feed"));
-  querySnapshot.forEach((doc) => {
-    var data = doc.data()
-    
-    // This is everything you need to make a post
-    var allergyList = data.allergy;
-    var caption = data.caption;
-    var location = data.location;
-    var photoLink = data.photo;
-    var rating = data.rating;
-  })
-}
+import { getDocs, collection } from "firebase/firestore";
 
 function FeedScreen() {
+  const [posts, setPosts] = useState([]);
+  async function retrievePosts() {
+    const querySnapshot = await getDocs(collection(firestore, "feed"));
+    let fireBaseResponse = [];
+    querySnapshot.forEach((doc) => fireBaseResponse.push(doc.data()));
+    setPosts(fireBaseResponse)
+  }
 
   useEffect(() => {
-    console.log("im here")
-    retrievePosts()
-  }, [])
+    retrievePosts();
+  }, []);
 
   return (
     <React.Fragment>
-      
       <TopNav />
       <div className="container">
-        <Post />
-        <Post />
+        {posts.map((data) => (
+          <Post />
+        ))}
       </div>
       <BotNav />
-      
     </React.Fragment>
   );
 }
