b0035c32a144847950167e1149e149b1c7991ad5
ally-nak <66540748+ally-nak@users.noreply.github.com>
2023-03-01 10:34:25 -0800

    checkin file fix

--- src/components/BotNav/index.js
+++ src/components/BotNav/index.js
@@ -2,7 +2,7 @@ import "./BotNav.css";
 import { NavLink } from "react-router-dom";
 import HomeIcon from "./home.svg";
 import PostIcon from "./post.svg";
-import checkIn from "./checkIn.svg";
+import checkIn from "./checkin.svg";
 import checkInSummary from "./checkInSummary.svg";
 
 function BotNav() {
