f0b33592423a45b8497fdad0594cc162af433fa1
zhuojl00 <72666211+zhuojl00@users.noreply.github.com>
2023-03-01 00:29:28 -0800

    updated ranked with states and no new screen

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -1,35 +1,43 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./TopNav.css";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import TopIcon from "./top.svg";
 import NewIcon from "./new.svg";
 import RankedIcon from "./ranked.svg";
 import PopupFilter from "../Filter/index.js";
 import Settings from "../Settings/index.js";
 
-function TopNav(props) {
+function TopNav({ fetchPost, retrievePosts }) {
+  const [ranked, setRanked] = useState(false);
+
   return (
-      <div className="TopNav">
-        <div className="logoWrapper">
-          <span className="logo">STANFORD GRUB</span>
-          <Settings />
-        </div>
-        <div className="buttonsWrapper">
-          <NavLink className="buttonWrapper" to="/top">
-            <img src={TopIcon} alt="Top Icon" />
-            <span className="buttonText">Top</span>
-          </NavLink>
-          <NavLink className="buttonWrapper" to="/new">
-            <img src={NewIcon} alt="New Icon" />
-            <span className="buttonText">New</span>
-          </NavLink>
-          <NavLink className="buttonWrapper" to="/ranked">
-            <img src={RankedIcon} alt="Ranked Icon" />
-            <span className="buttonText">Ranked</span>
-          </NavLink>
-          <PopupFilter />
-        </div>
+    <div className="TopNav">
+      <div className="logoWrapper">
+        <span className="logo">STANFORD GRUB</span>
+        <Settings />
       </div>
+      <div className="buttonsWrapper">
+        <NavLink className="buttonWrapper" to="/top">
+          <img src={TopIcon} alt="Top Icon" />
+          <span className="buttonText">Top</span>
+        </NavLink>
+        <NavLink className="buttonWrapper" to="/new">
+          <img src={NewIcon} alt="New Icon" />
+          <span className="buttonText">New</span>
+        </NavLink>
+        <div
+          onClick={() => {
+            ranked ? retrievePosts() : fetchPost();
+            setRanked(!ranked);
+          }}
+          className={ranked ? "buttonWrapper-clicked" : "buttonWrapper"}
+        >
+          <img src={RankedIcon} alt="Ranked Icon" />
+          <span className="buttonText">Ranked</span>
+        </div>
+        <PopupFilter />
+      </div>
+    </div>
   );
 }
 
