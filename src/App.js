5a5038d72ef1b2f0b007c90231a12cd73eb9e6c3
Ally Nakamura <allynak@stanford.edu>
2023-03-05 19:54:33 -0800

    adding user authentication via google sign in and login page and logout popup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import FeedScreen from "./screens/FeedScreen";
 import NewPostScreen from "./screens/NewPostScreen";
 import CheckInScreen from "./screens/CheckInScreen";
 import CheckInSummaryScreen from "./screens/CheckInSummaryScreen";
+import Login from "./screens/Login";
+import { auth } from "./firebase";
+
+function onAuthStateChange(setUserAuth) {
+  console.log("ENTERED AUTH CHANGE");
+  return auth.onAuthStateChanged(user => {
+    let val = null;
+    if (user) {
+      setUserAuth(user.uid);
+    } else {
+      setUserAuth(null);
+    }
+  })
+}
 
 export default function App() {
+
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    console.log("ENTERED USE EFFECT");
+    const loggedIn = onAuthStateChange(setAuthUser);
+    return () => {
+      loggedIn();
+    };
+  }, []);
+
+  if(!authUser) {
+    return <Login/>
+  }
+  
   return (
     <BrowserRouter className="App">
       <Routes>
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage} from "firebase/storage";
+import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult} from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyC6w2eVdlgM_EZdG3jzw8hT7LjKtpnaOOU",
@@ -14,3 +15,41 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   export const firestore = getFirestore(app)
   export const storage = getStorage(app);
+
+  /* GOOGLE SIGN-IN AND AUTH */
+  export const provider = new GoogleAuthProvider();
+  export const auth = getAuth(app);
+
+  export const signInWithGoogle = () => {
+    signInWithRedirect(auth, provider);
+  }
+
+  export const googleSignout = () => {
+    auth.signOut().then(function() {
+       console.log('Signout Succesfull')
+    }, function(error) {
+       console.log('Signout Failed')  
+    });
+ }
+
+  export const getUserInfo = () => {
+    console.log("ENTEREDD");
+    getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      console.log("results", result);
+      const user = result.user;
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+  }
+
